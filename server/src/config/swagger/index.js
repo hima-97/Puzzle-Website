228c843d82b6225a23f64279e92129ab2f6821ac
Tom <pnthoai165@gmail.com>
2022-10-26 18:03:42 -0700

    remove swagger, service

--- server/src/config/swagger/index.js
+++
@@ -1,8 +0,0 @@
-import { serve, setup } from "swagger-ui-express";
-import swaggerFile from "../../swagger_output.json";
-
-export default function (app, prefix) {
-  prefix = prefix || "";
-
-  app.use(prefix + "/api-docs", serve, setup(swaggerFile));
-}
