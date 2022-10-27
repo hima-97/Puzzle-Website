0ae19db828a4e7991d38bc6fa2fadae4862aea6b
Isaiah-Martinez <82429414+Isaiah-Martinez@users.noreply.github.com>
2022-10-26 17:16:46 -0700

    uncapitalization of a single letter. thought it was in the previous commit, my bad.

--- server/src/models/Puzzles.js
+++ server/src/models/Puzzles.js
@@ -1,7 +1,7 @@
 const mongoose  = require('mongoose');
 
 const PuzzleSchema = new mongoose.Schema({
-    //Outline of what defines a puzzle. Also looks like this in the database
+    //outline of what defines a puzzle. Also looks like this in the database
     name: {
         type: String,
         required: true,
