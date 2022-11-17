9e08d44c050e28d715b3b62c3dd57b491a7fd595
hima-97 <hima.kumar97@gmail.com>
2022-11-16 18:00:19 -0800

    Fixed some UI

--- client/src/components/recommendedPuzzlesComponent.js
+++ client/src/components/recommendedPuzzlesComponent.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { Component } from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import './recommendedPuzzlesComponent.css'
 
-export default function RecommendedPuzzlesComponent() {
-    return (
-        <div class="carousel-wrapper">
-            <Carousel infiniteLoop useKeyboardArrows autoPlay>
-                <div>
-                    <img src="img1.jpg" />
-                </div>
-                <div>
-                    <img src="img2.jpg" />
-                </div>
-                <div>
-                    <img src="img3.jpg" />
-                </div>
-            </Carousel>
-        </div>
-    );
+export default class RecommendedPuzzlesComponent extends Component {
+    render() {
+        return (
+            <div class="carousel-wrapper">
+                <Carousel infiniteLoop useKeyboardArrows autoPlay>
+                    <div>
+                        <img src="img1.jpg" />
+                    </div>
+                    <div>
+                        <img src="img2.jpg" />
+                    </div>
+                    <div>
+                        <img src="img3.jpg" />
+                    </div>
+                </Carousel>
+            </div>
+        );
+    }
 }
 
 
