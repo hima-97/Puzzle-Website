f925375bee7d31f641bca068e774c45af20af1be
Isaiah-Martinez <82429414+Isaiah-Martinez@users.noreply.github.com>
2022-11-28 00:49:12 -0800

    added ability to grab and display cat img from database

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -1,9 +1,27 @@
 // File for dashboard page
 
 import RecommendedPuzzlesComponent from "../components/recommendedPuzzlesComponent"
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function DashboardPage() {
+    const[image, updatedImage] = useState("");
+
+    axios.get('http://localhost:4002/getAllPuzzles')
+    .then(res => {
+      //console.log(res.data[0].img);
+      updatedImage(res.data[0].img);
+    }).catch(err => {
+      console.log(err);
+    })    
+
     return (
-        <RecommendedPuzzlesComponent />
+
+        <div>
+            <RecommendedPuzzlesComponent />
+            <img width="500" height="500" src={image} alt="idk"/>
+
+        </div>       
+        
     )
   }
